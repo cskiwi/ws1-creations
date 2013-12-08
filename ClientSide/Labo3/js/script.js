9db9c8a4781c14a54ef780c60a31cfb3c0556e61
cskiwi <glenn.latomme@gmail.com>
2013-12-08 14:19:07 +0100

    Labo 3 - bug fix ;) - overal cleanup - log if error

--- ClientSide/Labo3/js/script.js
+++ ClientSide/Labo3/js/script.js
@@ -7,10 +7,6 @@ var rows;
 var allCells;
 
 window.addEventListener('load', function() {
-    document.getElementById("myTable").addEventListener('blur', function(){
-        console.log("test");
-    });
-
     table = document.getElementById('myTable');
     rows = table.getElementsByTagName('tr');
     allCells = table.getElementsByTagName('td');
@@ -34,7 +30,6 @@ window.addEventListener('load', function() {
                 event.target.blur();
                 break;
             case 27:
-                console.log(event.target.restoreVal);
                 event.target.innerHTML = event.target.restoreVal;
                 event.target.blur();
                 break;
@@ -60,11 +55,11 @@ function commitValue(cell) {
     var blockrow, blockcol, rownr = cell.rownr+1, colnr = cell.colnr+1;
     while(rownr%3 != 0) rownr++;
     while(colnr%3 != 0) colnr++;
-    blockrow = rownr/3;
-    blockcol = colnr/3;
+    blockrow = rownr/3-1;
+    blockcol = colnr/3-1;
 
     // output number + block
-    console.log('' + blockrow + '(' + (cell.rownr+1) + ')' + ' x ' + blockcol + '(' + (cell.colnr+1) + ')');
+    console.log('' + cell.rownr + '(' + (blockrow) + ')' + ' x ' + cell.colnr + '(' + (blockcol) + ')');
 
     // check if valid
     for (var i = 0; i < rows.length; i++){
@@ -74,20 +69,30 @@ function commitValue(cell) {
             if(cells[j].rownr == cell.rownr){
                 if(cells[j].innerHTML == cell.innerHTML && cells[j] != cell && cell.innerHTML != ''){
                     cell.className = 'invalid';
+                    console.log('invalid with: ' + (cells[j].rownr) + ' x ' + (cells[j].colnr ));
                 }
             }
+
+            // skip check if already invalid
+            if (cell.className == 'invalid') return;
+
             // check col
             if(cells[j].colnr == cell.colnr){
                 if(cells[j].innerHTML == cell.innerHTML && cells[j] != cell && cell.innerHTML != ''){
                     cell.className = 'invalid';
+                    console.log('invalid with: ' + (cells[j].rownr) + ' x ' + (cells[j].colnr));
                 }
             }
 
+            // skip check if already invalid
+            if (cell.className == 'invalid') return;
+
             // check 3x3
-            if(cells[j].rownr >= 2*blockrow && cells[j].rownr < 2*blockrow +3){
-                if(cells[j].colnr >= 2*blockcol && cells[j].colnr < 2*blockcol +3){
+            if(cells[j].rownr >= 3*blockrow && cells[j].rownr < 3*blockrow +3){
+                if(cells[j].colnr >= 3*blockcol && cells[j].colnr < 3*blockcol +3){
                     if(cells[j].innerHTML == cell.innerHTML && cells[j] != cell && cell.innerHTML != ''){
                         cell.className = 'invalid';
+                        console.log('invalid with: ' + (cells[j].rownr) + ' x ' + (cells[j].colnr));
                     }
                 }
             }
