8427010c751753994a4b0841fc61e1d156c04f04
Glenn Latomme <glenn.latomme@gmail.com>
2014-01-28 10:40:35 +0100

    Update config.php

--- _Project/includes/config.php
+++ _Project/includes/config.php
@@ -23,7 +23,7 @@
 	define ('DB_USER', 'r0359502');
 
 	// Database Server Password
-	define ('DB_PASS', 'Cskiwi147963');
+	define ('DB_PASS', '');
 
 	// Database Name
 	define ('DB_NAME', 'r0359502_project');
